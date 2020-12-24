d7c53803f67fb4f51be2b810ca26d62bd2e9fa05
Сергей Резников <tikto01@mail.ru>
2020-12-24 13:53:33 +0400

    fixed

--- formpage.js
+++ formpage.js
@@ -1,9 +1,8 @@
-
 console.log("Скрипт загружен")
-let formData = new FormData();
+let formData = document.getElementById('formpageform');
 
-var buttonsubmit = document.forms[1].elements[7];
-buttonsubmit.addEventListener( "click" , buttonPressed);
+var buttonsubmit = document.getElementById('formbuttonsubmit');
+buttonsubmit.addEventListener( 'click' , buttonPressed);
 
 function buttonPressed(evt) {
 	console.log("Кнопка нажата загружен");
@@ -11,24 +10,24 @@ function buttonPressed(evt) {
 	
     let index=0;
     for(;index<6;index++){
-        if(document.forms[1].elements[index].value==""){
+        if(formData.elements[index].value==""){
             Swal.fire(
                 'Ошибка!',
                 'Вы не заполнили данные',
                 'error'
             )
             console.log("Вы не заполнили данные");
+            index=0;
             return;
         }
-        else{
-            formData.append(String(index),document.forms[1].elements[index].value);
+        else{     
+            index++;
         }
-        index++;
     }
-    if(document.forms[1].elements[6].checked==false){
+    if(formData.elements[6].checked==false){
         Swal.fire(
             'Ошибка!',
-            'Вы согласны.',
+            'Мы не можем принять даннее, пока вы не согласны с пользовательским соглашением.',
             'error'
         )
         return;
@@ -40,34 +39,34 @@ function buttonPressed(evt) {
         'success'
     )
         }
-    console.log(formData.get('0'),formData.get('1'),formData.get('2'),formData.get('3'),formData.get('4'),formData.get('5'));
 }
 
-var buttonsubmit = document.forms[0].elements[1];
-buttonsubmit.addEventListener( "click" , buttonPressed);
+let form = document.getElementById('indexform');
+var button = document.getElementById('indexbutton');
+button.addEventListener( "click" , Pressed);
 
-function buttonPressed(evt) {
+function Pressed(e) {
 	console.log("Кнопка нажата загружен");
-	evt.preventDefault();
+	e.preventDefault();
 	
-	if(document.forms[0].elements[0].value == ""){
+	if(indexform.elements[0].value == ""){
 	Swal.fire(
 			'Ошибка!',
 			'Вы не ввели запрос',
 			'error'
 		)
 		console.log("Вы не ввели запрос");
-	}
+    }
+    
 	else{
-		formData.append('Search', document.forms[0].elements[0].value);
+        let string = indexform.elements[0].value;
 		Swal.fire({
 			position: "center",
 			icon: "success",
-			title: "Ваше запрос " + formData.get("Search"),
+			title: "Ваше запрос " + string,
 		}
 		)
 		setTimeout(function() {document.location.href = "listpage.html";},1500);
-		console.log("Перешли на страницу поиска");
 	}
 }
 
